2dd408fcbb6db32e373fbb40130eea3ac6844d27
WaddlingLard <dathamor@gmail.com>
2026-06-03 18:46:13 -0600

    initial btree commit

---
+++ Btree.py
@@ -0,0 +1,13 @@
+class Btree:
+
+    def __init__(self):
+        self.root = None
+
+    def output_tree: -> str
+        print(self.root or 'Tree is empty')
+
+if __name__ == '__main__':
+    print('Hello, World!')
+
+    btree = Btree()
+    
