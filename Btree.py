6a22f64c7a7f84e58285ece30c6ff0cc13e2c0fa
WaddlingLard <dathamor@gmail.com>
2026-06-04 01:56:23 -0600

    added type defs to instance vars and a lil more code :)

--- Btree.py
+++ Btree.py
@@ -1,13 +1,22 @@
+from BtreeNode import BtreeNode
+import math
+
 class Btree:
 
-    def __init__(self):
-        self.root = None
+    def __init__(self, number_of_keys):
+        self.root: BtreeNode = BtreeNode(number_of_keys)
+        self.max_keys: int = number_of_keys
+        self.min_keys: int = math.floor(number_of_keys / 2)
 
-    def output_tree: -> str
+    def insert(self, value):
+        pass
+
+    def output_tree(self) -> str:
         print(self.root or 'Tree is empty')
 
 if __name__ == '__main__':
     print('Hello, World!')
 
-    btree = Btree()
+    btree = Btree(4)
+    btree.output_tree()
     
