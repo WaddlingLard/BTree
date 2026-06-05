16e5a34902efca97693026039ae071018d1777df
WaddlingLard <dathamor@gmail.com>
2026-06-05 15:22:05 -0600

    refactored insert method, unneeded code commented out

--- BtreeNode.py
+++ BtreeNode.py
@@ -7,26 +7,30 @@ class BtreeNode:
 
     def insert(self, item) -> bool:
 
-        if self.size == len(self.node):
-            # Splitting nodes should be handled at Btree level
-            print('Split Node Operation')
-            return False
+        # Size check, cannot add more than limit but should consider to handle in Btree instead of here
+        # if self.node_size == len(self.node):
+        #     # Splitting nodes should be handled at Btree level
+        #     print('Split Node Operation')
+        #     return False
 
         # Empty node
-        if len(self.node) == 0:
-            self.node.append(item)
-            return True
+        # if len(self.node) == 0:
+        #     self.node.append(item)
+        #     return False
 
         # Iterate through list to find valid location   
         current_index = 0
-        while self.node[current_index] <= item:
+        while current_index < len(self.node) and self.node[current_index] <= item:
             if item != self.node[current_index]:
                 current_index += 1
             else:
+                # Maybe implement proper error handling, but for now its sufficient 
                 print('Inserting Duplicate Key! Failed Insertion')
-                return False
+                return False 
         self.node.insert(current_index, item)
-        return True
+
+        # Do we need to split?
+        return self.node_size > len(self.node)
 
     def get_size(self) -> int:
         return self.size
