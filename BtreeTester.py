9d8cbe047ba9213146e67b610f3efef7b0df47f3
WaddlingLard <dathamor@gmail.com>
2026-06-05 17:09:48 -0600

    added tests for binary_search

--- BtreeTester.py
+++ BtreeTester.py
@@ -1,5 +1,54 @@
 from Btree import Btree
 from BtreeNode import BtreeNode
+from binary_search import binary_search
+
+# -------------------------------
+## UTILITY TESTS
+# -------------------------------
+def test_search_single_element_success():
+    vals: list[int] = [1]
+
+    assert binary_search(vals, 1) == 0
+
+# def test_search_single_element_failure():
+#     vals: list[int] = [1]
+
+#     assert binary_search(vals, 2) == -1
+
+def test_search_single_element_failure():
+    vals: list[int] = [2]
+
+    assert binary_search(vals, 1) == -1
+
+def test_search_double_element_success():
+    vals: list[int] = [1,2]
+
+    assert binary_search(vals, 2) == 1
+
+def test_search_double_element_failure():
+    vals: list[int] = [1,2]
+
+    assert binary_search(vals, 3) == -1
+
+def test_search_multi_element_success():
+    vals: list[int] = [1,2,3]
+    
+    assert binary_search(vals, 3) == 2
+
+def test_search_multi_element_failure():
+    vals: list[int] = [1,2,3]
+    
+    assert binary_search(vals, 4) == -1
+
+def test_search_many_element_success():
+    vals: list[int] = [1,2,3,4,5,6,7,8,9,10]
+    
+    assert binary_search(vals, 10) == 9
+
+def test_search_many_element_failure():
+    vals: list[int] = [1,2,3,4,5,7,8,9,10,11]
+    
+    assert binary_search(vals, 6) == -1
 
 # -------------------------------
 ## BTREE TESTS
