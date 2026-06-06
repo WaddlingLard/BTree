68f1a453d77cd39219d430323a3bbabae6dfffbc
WaddlingLard <dathamor@gmail.com>
2026-06-06 04:18:05 -0600

    implemented basic splitting logic for inital split, need to do for general case (with recursion)

--- Btree.py
+++ Btree.py
@@ -18,6 +18,31 @@ class Btree:
         return search(self.root.get_node_contents(), value) != -1
         # Note: Need to alter search when it fails to find the last value it attempted searching
 
+    def insert(self, value, disable_split: bool = False):
+
+        # Insertion should look for deepest node
+        invariant_check = self.root.insert(value) 
+
+        if disable_split:
+            # print('split disabled')
+            return
+
+        if not invariant_check:
+            # Have to split root (What if not in root though?)
+
+            # Keep current root? (hold left side values)
+            # Make 2 more nodes (middle for new root, right for right child)
+            extracted_keys: list[any] = self.root.split(self.min_keys)
+            
+            # Get the child nodes
+            right_child: BtreeNode = BtreeNode(self.max_keys, extracted_keys[1:])
+            left_child: BtreeNode = self.root
+            
+            # Create the parent node
+            new_root: BtreeNode = BtreeNode(self.max_keys, extracted_keys[:1], False, [left_child, right_child])
+
+            # Set the new parent node
+            self.root = new_root
 
     def output_tree(self) -> str:
         print(self.root or 'Tree is empty')
