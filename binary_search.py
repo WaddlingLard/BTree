5e7c8d96fa5a170cfb795641ffb19d277c19d2b1
WaddlingLard <dathamor@gmail.com>
2026-06-07 01:20:15 -0600

    cleaned up search and unneeded code

--- binary_search.py
+++ binary_search.py
@@ -7,12 +7,12 @@ def binary_search(list_of_vals, searching_for) -> int:
     
     upper = len(values) - 1
     lower = 0
+
+    # Edge Case: Singleton
+    if len(values) == 1:
+        return 0 if values[0] == searching_for else -1
     
     while True:
-
-        # Edge Case: Singleton
-        if len(values) == 1:
-            return 0 if values[0] == searching_for else -1
 
         middle = math.floor((upper + lower) / 2)
         if (upper < lower):
@@ -23,13 +23,10 @@ def binary_search(list_of_vals, searching_for) -> int:
             # Search Bottom Half
             upper = middle - 1       
         else:
-            lower = middle + 1
             # Search Top Half
-            # if len(values) <= 2:
-            #     # Edge case: 1-2 element lists
-            #     lower += 1
+            lower = middle + 1
 
-if __name__ == '__main__':
-    numbers = [1,2,3,4,5,6,7,8,9,10]
+# if __name__ == '__main__':
+#     numbers = [1,2,3,4,5,6,7,8,9,10]
 
-    print(binary_search(numbers, 7))
+#     print(binary_search(numbers, 7))
